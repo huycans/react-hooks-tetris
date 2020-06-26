33a4a030b02d4c8e2a3ec9bff6470526b4b52fb4
huycans <huycan_lk@yahoo.com.vn>
2020-06-26 11:35:55 -0400

    Fix minor style

--- src/components/styles/StyledStage.js
+++ src/components/styles/StyledStage.js
@@ -6,15 +6,10 @@ export const StyledStage = styled.div`
     ${props => props.height},
     calc(25vw / ${props => props.width})
   );
-  grid-template-columns: repeat(
-    ${props => props.width},
-    1fr
-  );
+  grid-template-columns: repeat(${props => props.width}, 1fr);
   grid-gap: 1px;
   border: 2px solid #333;
-  min-width: 200px;
   width: 100%;
   max-width: 25vw;
-  background: #111; 
-  
-`
+  background: #111;
+`;
